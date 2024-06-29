c915d859d4de43bc3699a4664cecfc1cf95388be
Gulidov Viktor <gugloses@gmail.com>
2024-06-29 22:42:34 +0300

    add mova

---
+++ lib/mova.php
@@ -0,0 +1,60 @@
+<?php
+
+// Бібліотека з усім щостосується перекладу сайту
+
+class mova {
+
+    // Функція, що допомагає вибрати мову перекладу сайту із закладених
+    // Надто просто, але не страшно поки мов мало
+    // В першу чергу намагаємось взяти мову партнеру
+    // Якщо її немає, то мову з кукісів
+    // Якщо її немає, то по ip
+    // І у випадку форсмажорів різних беремо мову сайту
+    static function alang($partnerlg,$CountryByIP,$cookielg,$sitelg = false) {
+
+        if(!empty($partnerlg)){
+            $aco = $partnerlg;
+        } else if(!empty($cookielg)){
+            $aco = $cookielg;
+        } else if(!empty($_COOKIE['lang'])){
+            $aco = $_COOKIE['lang'];
+        } else if(!empty($CountryByIP)){
+            $aco = $CountryByIP;
+        } else {
+            $aco = $sitelg;
+        }
+        // В перспективе дефолтным при форсмажорах должен быть en конечно же
+        if(empty($aco) or $aco == 'ua' or $aco == 'uk'){
+            return 'uk'; // ukraine - вимушений використовувати саме uk
+        }
+        if($aco == 'ru' or $aco == 'ge' or $aco == 'by' or $aco == 'kz'){
+            return 'ru';
+        }
+        if($aco == 'en' or $aco == 'gb'){
+            return 'en';
+        }
+        return $sitelg;
+
+    }
+
+    // Спосіб обрати один з варіантів перекладу
+    static function lg($lg,$ua,$ru = false,$en = false) {
+        // вважаємо, що Українська мова основна і не порожня за жодних обставин
+        if($lg == 'uk' or $lg == 'ua'){
+            return $ua;
+        }
+
+        if($lg == 'ru' and !empty($ru)){
+            return $ru;
+        }
+        if($lg == 'en' and !empty($en)){
+            return $en;
+        }
+        // У цьому випадку, у будь-якій незрозумілій ситуації
+        // видаємо $ua переклад
+        return $ua;
+    }
+
+
+} // class lg
+
